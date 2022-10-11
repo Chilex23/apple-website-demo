dc5f41be9ea1725a9ff16638b2e9d3cb1a9924e6
Chilex23 <onumaegbuchilex@gmail.com>
2022-10-11 22:25:24 +0100

    Added images

--- components/hero.js
+++ components/hero.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="mt-4 col-span-full tablet:col-span-4 ">
-      <Image priority src="/headshot.jpg" alt="hero" width={850} height={400} />
+      <Image priority src="https://i.ibb.co/XzcwL5s/black-shearling.png" alt="hero" width={850} height={400} />
     </div>
   );
 }
