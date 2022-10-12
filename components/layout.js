15b51a9dd82e2035aba4f513b6723222f26f4f57
Chilex23 <onumaegbuchilex@gmail.com>
2022-10-12 13:43:40 +0100

    Added icons and changed the font-family

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,9 @@ export default function Layout({ children }) {
     <div className="mt-4">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" /> 
         <meta
           name="description"
           content="Apple website provides a great shopping experience"
--- components/sidebar.js
+++ components/sidebar.js
@@ -1,12 +1,28 @@
 import Link from "next/link";
+import { IoManSharp, IoWoman } from "react-icons/io5";
+import { FaChild, FaGem } from "react-icons/fa";
+
+function getIcon(name) {
+  const icons = {
+    "Men": <IoManSharp className="text-2xl" />,
+    "Women": <IoWoman className="text-2xl" />,
+    "Children": <FaChild className="text-2xl" />,
+    "Jewelry": <FaGem className="text-2xl" />
+  }
+
+  return icons[name];
+}
 
 export default function SideBar({ categories }) {
   return (
-    <div className="border-2 border-black mt-4 p-2 rounded-md col-span-full tablet:col-span-1">
+    <div className="border-2 border-black mt-4 px-2 py-4 rounded-md col-span-full tablet:col-span-1">
       <div className="flex flex-col justify-between h-full">
         {categories.map(({ id, name }) => (
           <Link href={`/category/${id}`} key={id}>
-            <a>{name}</a>
+            <a className="flex items-center gap-x-3 hover:bg-gray-300 py-3 px-1 rounded-md">
+              {getIcon(id)}
+              <span>{name}</span>
+            </a>
           </Link>
         ))}
       </div>
